c617a5b6bbf9fa2869fd1240ecbaaed30807b1b4
romio-krvtsky <79285410+romio-krvtsky@users.noreply.github.com>
2021-04-08 14:04:38 +0300

    Add files via upload

---
+++ lab 3(2.0)/src.h
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+#pragma once
+void EnterAndCheckInput(int* a);
+void FillArrayWithZerosAndOnes(int** a, int n, int m );
+void DeleteDuplicateRows(int** a, int n, int m);
+void ChangeElementsOrder(int** a, int n, int m);
+void ShowArray(int** a, int n, int m);
+=======
+#pragma once
+void EnterAndCheckInput(int* a);
+void FillArrayWithZerosAndOnes(int** a, int n, int m );
+void DeleteDuplicateRows(int** a, int n, int m);
+void ChangeElementsOrder(int** a, int n, int m);
+void ShowArray(int** a, int n, int m);
+>>>>>>> f37ec5c15471ddacff8cc8a5e7c560b6f888c6f3
