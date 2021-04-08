e7561475784bdfad4bac41857da9b22efb297913
romio-krvtsky <79285410+romio-krvtsky@users.noreply.github.com>
2021-04-08 14:04:28 +0300

    Delete src.h

--- lab 3(2.0)/src.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-void EnterAndCheckInput(int* a);
-void FillArrayWithZerosAndOnes(int** a, int n, int m );
-void DeleteDuplicateRows(int** a, int n, int m);
-void ChangeElementsOrder(int** a, int n, int m);
-void ShowArray(int** a, int n, int m);
