ee252ebfc0527f6a747f96f9e3d19061cd6f0163
romio-krvtsky <79285410+romio-krvtsky@users.noreply.github.com>
2021-04-26 00:57:42 +0300

    go

---
+++ lab 5/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "queue.h"
+
+int main() {
+	MyQueue q1(5);
+	q1.Push(1);
+	q1.Push(4);
+	q1.Push(5);
+	q1.Push(10);
+	q1.Push(99);
+	q1.Pop();
+	q1.Pop();
+	q1.Print();
+	std::cout << std::endl;
+	q1.Inversion();
+	q1.Print();
+	//q1.Inversion();
+	std::cout << std::endl;
+	std::cout << q1.First() << '\t' << q1.Last();
+
+}
---
+++ lab 5/queue.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include<iostream>
+
+class MyQueue {
+
+private:
+	int* arr;
+	int capacity;
+	int size;
+	int first;
+	int last;
+
+public:
+
+	MyQueue(int cap) {
+		capacity = cap;
+		arr = new int[capacity];
+		size = 0;
+		first = 0;
+		last = -1;
+	}
+
+	bool Empty() {
+		return (size == 0);
+	}
+
+	void Push(int value) {
+		if (size >= capacity) {
+			std:: cout <<"Queue is full!";
+		}
+		else {
+			arr[++last] = value;
+			size++;
+		}
+	}
+
+	void Pop() {
+		if (Empty()) {
+			std::cout << "queue is already empty.";
+		}
+		else {
+			size--;
+			last--;
+			int* newArr = new int[size];
+			for (int i = 0; i < size; ++i) {
+				newArr[i] = arr[i + 1];
+			}
+			delete[]arr;
+			arr = newArr;
+		}
+	}
+
+	int First() {
+		if (Empty()) {
+			throw std::exception("queue is empty!");
+		}
+		else {
+			return arr[first];
+		}
+	}
+
+	int Last() {
+		if (Empty()) {
+			throw std::exception("queue is empty!");		
+		}
+		else {
+			return arr[last];
+		}
+	}
+
+	void Inversion() {
+		int* inversArr = new int[capacity];
+		for (int i = 0; i < size; ++i) {
+			inversArr[i] = arr[size - 1 - i];
+		}
+		delete[] arr;
+		first = 0;
+		arr = inversArr;
+	}
+
+	void Print() {
+		for (int i = 0; i < size; ++i) {
+			std::cout << arr[i] << ' ';
+		}
+	}
+
+	/*int& operator[](int index) {
+		if (index >= 0) {
+			return arr[index];
+		}
+		else throw std::exception("error!");
+	}*/
+
+	~MyQueue(){
+		delete[] arr;
+		size = 0;
+	}
+};
+ 
