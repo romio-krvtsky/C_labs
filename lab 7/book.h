990cce1a54fd0980891a4a8a76a4500e5f97fbfa
Romio <79285410+romio-krvtsky@users.noreply.github.com>
2021-05-19 02:01:40 +0300

    fix

--- lab 7/book.h
+++ lab 7/book.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <windows.h>
 
 const int max_str_length = 25;
 const int radix = 10;
--- lab 7/lab7.cpp
+++ lab 7/lab7.cpp
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <windows.h>
 #include "book.h"
 #include "menu.h"
 
